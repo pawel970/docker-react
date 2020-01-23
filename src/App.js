264e9cc45b6f05fc12bb26ad157766f98678b981
pawel970 <pawel970@o2.pl>
2020-01-23 20:52:07 +0100

    changed text on app.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi there OK Plus Fature
+          Hi there OK Plus Second Time OK OK
         </p>
         <a
           className="App-link"
