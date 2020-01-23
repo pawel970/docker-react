f047dd3828ee3b67476801eebf3f587d42a82cce
pawel970 <pawel970@o2.pl>
2020-01-23 20:45:45 +0100

    changed app.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi there OK
+          Hi there OK Plus Fature
         </p>
         <a
           className="App-link"
